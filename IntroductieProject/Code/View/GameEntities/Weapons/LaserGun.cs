5cb1c5df77388976f98d585a00c4b33e9f3af64d
Lars Puijpe <l.puijpe@students.uu.nl>
2022-12-02 14:33:44 +0100

    bruh

--- IntroductieProject/Code/View/GameEntities/Weapons/LaserGun.cs
+++ IntroductieProject/Code/View/GameEntities/Weapons/LaserGun.cs
@@ -11,7 +11,7 @@ namespace IntroductieProject.Code.View.GameEntities.Weapons
         public LaserGun(Vector2 center, int width, int height, string assetName, string bulletSpriteName) : base(center, width, height, assetName, bulletSpriteName)
         {
             weaponFireRate = 100;
-            shotSpeed = 0;
+            shotSpeed = 100;
             projectileSize = 20;
             gunRange = 3000;
             spreadStrength = 0;
@@ -21,7 +21,7 @@ namespace IntroductieProject.Code.View.GameEntities.Weapons
         {
             if (canShoot)
             {
-                projectiles.Add(new Projectile(centerPosition + new Vector2(width/2, 0), (int)(100 + 0.10 * projectileSize), (int)(100 + 0.10 * projectileSize), addSpread(new Vector2(InputManager.MouseState.Position.X - centerPosition.X - width / 2, InputManager.MouseState.Position.Y - centerPosition.Y - height / 2), spreadStrength), shotSpeed, 5, spriteName, gunRange));
+                projectiles.Add(new Projectile(centerPosition + new Vector2(width/2, height/2), (int)(100 + 0.10 * projectileSize), (int)(10 + 0.10 * projectileSize), addSpread(new Vector2(InputManager.MouseState.Position.X - centerPosition.X - width / 2, InputManager.MouseState.Position.Y - centerPosition.Y - height / 2), spreadStrength), shotSpeed, 5, spriteName, gunRange));
                 shootCooldown = (1 / weaponFireRate) * 1000;
                 canShoot = false;
             }
