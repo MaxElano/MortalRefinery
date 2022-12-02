b548c14a575f1cc273509b2dfa0f02a2ef212d5e
Luuk van Doorn <l.vandoorn@students.uu.nl>
2022-12-02 14:23:59 +0100

    almost fixed laser rotation

--- IntroductieProject/Code/View/GameEntities/Projectile.cs
+++ IntroductieProject/Code/View/GameEntities/Projectile.cs
@@ -46,9 +46,10 @@ namespace IntroductieProject
         {
             RotationInDegress = (float)Math.Atan2(direction.X, direction.Y);
 
-            RotationInDegress += 90 ;
-
             RotationInDegress = MathHelper.ToDegrees(RotationInDegress);
+
+            RotationInDegress += 90;
+
             return -RotationInDegress;
 
         }
@@ -56,6 +57,16 @@ namespace IntroductieProject
         protected override void Die(GameEntity entity)
         {
             base.Die(entity);
+        }
+
+        internal override void draw(SpriteBatch batch)
+        {
+            batch.Draw(sprite, getBoundingBox(), null, Color.White, MathHelper.ToRadians(RotationInDegress), new Vector2(-width / 2, 0), spriteEffect, 0f);
+        }
+
+        internal override Rectangle getBoundingBox()
+        {
+            return new Rectangle((int)this.centerPosition.X, (int)this.centerPosition.Y, width, height);
         }
     }
 }
